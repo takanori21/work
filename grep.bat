d8d437d005e95abcd12a6024e037a751cd0a6612
takanori21 <taka63.121@gmail.com>
2020-04-19 20:39:41 +0900

    新規登録

---
+++ grep.bat
@@ -0,0 +1,6 @@
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=hoge -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=foo -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=bar -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=hogehoge -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=foofoo -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=barbar -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
