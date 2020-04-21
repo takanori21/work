71fb7e6c0d0faa1b3a6a79ff571e72e6c3532652
takanori21 <taka63.121@gmail.com>
2020-04-22 01:01:25 +0900

    ■バグ修正 サクラエディタのGREPオプションの違反箇所修正 　・"-GKEY","-GFILE","-GFOLDER"に設定する値に""を付与 　・"-GKEY"に設定する値内に"がある場合、""に置き換えるよう修正 関数get_result_info内で使用する正規表現の修正(最短マッチで意図した値が取得できなかったため)

--- grep.bat
+++ grep.bat
@@ -1,6 +1,9 @@
-"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=hoge -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
-"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=foo -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
-"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=bar -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
-"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=hogehoge -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
-"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=foofoo -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
-"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY=barbar -GFILE=!*.xlsx,*.* -GFOLDER=C:\Users\taka6\Desktop\work_repo\hoge -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="hoge" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="foo" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="bar" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="hogehoge" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="foofoo" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="barbar" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="""hoge""" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="""hog""e""" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
+"C:\Program Files (x86)\sakura\sakura.exe" -GREPMODE -GKEY="""ho""g""e""" -GFILE="!*.xlsx,*.* " -GFOLDER="C:\Users\taka6\Desktop\work_repo\hoge" -GOPT=SRWU >> C:\Users\taka6\Desktop\work_repo\output\result_all.txt
